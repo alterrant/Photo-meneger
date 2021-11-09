170a706a63af6984ac53b586acaf6e9b17ed8624
alterrant <pandor246@mail.ru>
2021-11-09 20:40:05 +0300

    add: testing deploy with todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import UserTitile from "./Components/UserTitile/UserTitile";
+import {PhotoLoader} from "./Components/PhotoLoader/PhotoLoader";
+import {Photos} from "./Components/Photos/Photos";
+import {SelectedPhoto} from "./Components/SelectedPhoto/SelectedPhoto";
+import {useState} from "react";
+
+// TODO make personalise pictures and h1 description
 
 function App() {
+
+  let [selectedPhoto, setSelectedPhoto] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='main-wrapper'>
+        <UserTitile />
+        <PhotoLoader fill={"#8A2BE2"} stroke={"#8A2BE2"}/>
+        <Photos setSelectedPhoto={setSelectedPhoto}/>
+        {selectedPhoto && <SelectedPhoto selectedPhoto={selectedPhoto} setSelectedPhoto={setSelectedPhoto}/>}
+      </div>
   );
 }
 
