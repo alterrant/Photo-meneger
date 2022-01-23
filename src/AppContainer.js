d69a04b8273f2a9b8695630082834b3f281cb757
alterrant <pandor246@mail.ru>
2022-01-23 23:19:11 +0300

    refactor: replace connect to hooks

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,22 +1,24 @@
 import './App.css';
-import {Profiler, useEffect, useState} from "react";
+import {useEffect} from "react";
 import Auth from "./Components/Auth/Auth";
 import {getAuth, onAuthStateChanged} from "firebase/auth";
-import {connect} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {initialize} from "./redux/initialiseApp"
 import Preloader from "./Components/common/Preloader/Preloader";
-import UserPhotos from "./Components/UserProfile/UserProfile";
 import App from "./App";
 
-function AppContainer ({isAuth, isInit, initialize}) {
+function AppContainer () {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(state => state.auth.isAuth);
+  const isInit = useSelector(state => state.initialiseApp.isInitialized);
 
   useEffect(() => {
     const auth = getAuth();
-    onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        initialize(user);
+    onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        dispatch(initialize(currentUser));
       } else {
-        initialize();
+        dispatch(initialize());
       }
     })
   }, [])
@@ -33,11 +35,4 @@ function AppContainer ({isAuth, isInit, initialize}) {
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    isAuth: state.auth.isAuth,
-    isInit: state.initialiseApp.isInitialized
-  }
-}
-
-export default connect(mapStateToProps, {initialize})(AppContainer);
+export default AppContainer;
--- src/Components/Auth/Sign out/SignOut.jsx
+++ src/Components/Auth/Sign out/SignOut.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import {connect} from "react-redux";
-import {logOut} from "../../../redux/authReducer";
+import {useDispatch} from "react-redux";
+import {loggingOut} from "../../../redux/authReducer";
 import {motion} from "framer-motion";
 
-const LogOut = ({logOut}) => {
+const LogOut = () => {
+  const dispatch = useDispatch();
+
   return (
       <div>
         <motion.button className="buttonLogOut"
                        whileHover={{
                          boxShadow: `3px 3px 3px rgba(63, 44, 138, 0.49)`
                        }}
-                       onClick={logOut}>Sign Out</motion.button>
+                       onClick={() => dispatch(loggingOut())}>Sign Out</motion.button>
       </div>
   )
 }
 
-export default connect(null, {logOut})(LogOut);
+export default LogOut;
