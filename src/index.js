1a2a2d999ae786ac9bb4a558b5b013d2871de9d3
alterrant <pandor246@mail.ru>
2022-01-23 23:30:17 +0300

    refactor: make store export by default

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {Provider} from "react-redux";
-import {store} from "./redux/store";
 import {BrowserRouter} from "react-router-dom";
 import AppContainer from "./AppContainer";
+import store from "./redux/store";
 
 ReactDOM.render(
     <React.StrictMode>
